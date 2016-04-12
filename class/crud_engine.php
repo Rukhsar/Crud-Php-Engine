5d3ee44e766f9cb5e0c6fdd7a57a5aac0c10ab72
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-04-12 10:18:31 +0100

    variables defined

--- class/crud_engine.php
+++ class/crud_engine.php
@@ -9,6 +9,19 @@ class CrudEngine
 {
 
     /*
+     * Create variables for credentials to MySQL database
+     * The variables have been declared as private. This
+     * means that they will only be available with the
+     * CrudEngine class
+     */
+
+    private $db_host = "localhost"; // Database Server Change as required
+    private $db_user = "user";      // Database Username Change as required
+    private $db_pass = "password";  // Database Password Change as required
+    private $db_name = "database";  // Database Name Change as required
+
+
+    /*
     *   To Do
     *   Define Variables
     *   Create All Functions

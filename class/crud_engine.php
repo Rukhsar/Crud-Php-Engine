e68a09e495fdcba00fa89839707fa0c55bf31a0b
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-04-12 09:57:24 +0100

    class createde

---
+++ class/crud_engine.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @Author Rukhsar Manzoor <rukhsar.man@gmail.com>
+ * @Version 1.0
+ * @Package CrudEngine
+ */
+
+class CrudEngine
+{
+
+    function __construct(argument)
+    {
+
+    }
+}
+
+
+?>
